37fd95f1548345ad69a687ef78aaef65d4104823
Spice <kf.shared.ptr@gmail.com>
2023-05-25 00:08:20 -0500

    Added NolangCallable to py_type_to_nl

--- nolang/util.py
+++ nolang/util.py
@@ -1,5 +1,6 @@
 
 from types import NoneType
+from .runtime.callables import NolangCallable
 
 def is_digit(c: str):
     return ord('0') <= ord(c) <= ord('9') if c else False
@@ -10,12 +11,14 @@ def is_alpha(c: str):
 def is_alpha_numeric(c: str):
     return is_digit(c) or is_alpha(c) if c else False
 
+# TODO: Need to define custom object type and use __str__ for this!!!
 def py_type_to_nl(t: type):
     if t is int: return 'int'
     elif t is float: return 'float'
     elif t is str: return 'string'
     elif t is bool: return 'bool'
     elif t is NoneType: return 'nol'
+    elif t is NolangCallable: return 'greg'
     else: raise Exception(f'Unknown nolang type {t}')
 
 def stringify(val) -> str:
