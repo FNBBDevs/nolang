6925504926020aea8376f8633a10e0f297800a17
Spice <kf.shared.ptr@gmail.com>
2023-05-26 03:50:29 -0500

    Fix built-in functions returning wrong type

--- nolang/runtime.py
+++ nolang/runtime.py
@@ -22,21 +22,21 @@ class Nolin(NolangCallable):
         return 1
 
     def __call__(self, _, args: list[NolangType]):
-        return input(args[0])
+        return NolangString(input(args[0]))
 
 class Time(NolangCallable):
     def arity(self) -> int:
         return 0
 
     def __call__(self, *_):
-        return int(round(time.time() * 1000))
+        return NolangInt(round(time.time() * 1000))
 
 class Random(NolangCallable):
     def arity(self) -> int:
         return 0
 
     def __call__(self, *_):
-        return random.random()
+        return NolangFloat(random.random())
 
 class Int(NolangCallable):
     def arity(self) -> int:
@@ -44,10 +44,11 @@ class Int(NolangCallable):
 
     def __call__(self, _, args: list[NolangType]):
         try:
-            return int(args[0])
+            return NolangInt(args[0])
+
         except ValueError:
             # klim, implement error, thanks, or I can later
-            return 420
+            return NolangInt(420)
 
 class Float(NolangCallable):
     def arity(self) -> int:
@@ -55,10 +56,11 @@ class Float(NolangCallable):
 
     def __call__(self, _, args: list[NolangType]):
         try:
-            return float(args[0])
+            return NolangFloat(args[0])
+
         except ValueError:
             # klim, implement error, thanks, or I can later
-            return 420.0
+            return NolangFloat(420.0)
 
 class RoundDown(NolangCallable):
     def arity(self) -> int:
@@ -66,9 +68,10 @@ class RoundDown(NolangCallable):
 
     def __call__(self, _, args: list[NolangType]):
         try:
-            return int(args[0])
+            return NolangInt(args[0])
+
         except ValueError:
-            return args[0]
+            return NolangString(args[0])
 
 class RoundUp(NolangCallable):
     def arity(self) -> int:
@@ -76,9 +79,10 @@ class RoundUp(NolangCallable):
 
     def __call__(self, _, args: list[NolangType]):
         try:
-            return int(math.ceil(args[0]))
+            return NolangInt(math.ceil(args[0]))
+
         except ValueError:
-            return args[0]
+            return NolangString(args[0])
 
 # Global runtime, this should be immutable!
 
