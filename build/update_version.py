9d20636ab6dddca898d932bc9afaf352b70d3f98
ethanlchristensen <ethanlchristensen@outlook.com>
2023-10-08 16:26:57 -0500

    update

--- build/update_version.py
+++ build/update_version.py
@@ -16,17 +16,17 @@ if re.match(r"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+", new_version) == None:
 """
 Update the version.yml file to change the version number
 """
-with open("./version.yml", "r", encoding="utf8") as version_file:
+with open(r"D:\a\nolang\nolang\build\version.yml", "r", encoding="utf8") as version_file:
     version_file_lines = version_file.readlines()
     version_file_lines[0] = f"Version: {new_version}\n"
 
-with open("./version.yml", "w", encoding="utf8") as version_file:
+with open(r"D:\a\nolang\nolang\build\version.yml", "w", encoding="utf8") as version_file:
     version_file.writelines(version_file_lines)
 
 """
 Update the nolang.py file to change the version number
 """
-with open("../nolang.py",  "r", encoding="utf8") as nolang_file:
+with open(r"D:\a\nolang\nolang\nolang.py",  "r", encoding="utf8") as nolang_file:
     lines = nolang_file.readlines()
     for idx, line in enumerate(lines):
         if line[:2] == "__":
@@ -35,5 +35,5 @@ with open("../nolang.py",  "r", encoding="utf8") as nolang_file:
             line = " ".join(line)
             lines[idx] = line+"\n"
             break
-with open("../nolang.py", "w", encoding="utf8") as nolang_file:
+with open(r"D:\a\nolang\nolang\nolang.py", "w", encoding="utf8") as nolang_file:
     nolang_file.writelines(lines)
